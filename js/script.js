cecf74f9b8e6b18e6dfc213b2f2f32e24993fb9f
Osman Armando Jimenez Cortes <33326926+OsmanJimenez@users.noreply.github.com>
2020-10-26 16:07:37 -0500

    Revert "Added show passwords"
    This reverts commit 04cc8b2ac351a1a4ffbcad5ebd6509634bdb26f2.

--- js/script.js
+++ js/script.js
@@ -104,23 +104,3 @@ formulario.addEventListener('submit', (e) => {
 		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
 	}
 });
-function mostrar(){
-  
- 
- const pass1=document.getElementById("password");
- const pass2=document.getElementById("password2");
- const aparecer=document.getElementById("ver");
-
-
-  if(aparecer.className=="fas fa-eye"){
-      pass1.type = "text";
-      pass2.type = "text";
-      aparecer.className="fas fa-eye-slash";
-  } else {
-  	  pass1.type = "password";
-      pass2.type = "password";
-      aparecer.className="fas fa-eye";
-  }
-
-
-}
