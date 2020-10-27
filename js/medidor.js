74f82eb4c8603aec651ac4fa5ec973bbd9f247dd
Osman Armando Jimenez Cortes <33326926+OsmanJimenez@users.noreply.github.com>
2020-10-27 15:39:29 -0500

    Fix_Level_Security

--- js/medidor.js
+++ js/medidor.js
@@ -11,6 +11,10 @@ $(function() {
 	  nivel;
   
 	function devuelveNivel(esteInput, evento) {
+
+		let passInput = document.getElementById("password").value;
+		  password = passInput;
+
 	  var nivelBajo = 8,
 		nivelMedio = 12,
 		nivelAlto = 16,
@@ -19,20 +23,29 @@ $(function() {
 		espanNivelesColores = $(".spanNivelesColores");
 	  evento.stopPropagation();
 	  limpiarError();
+
+	  /* Niveles de Seguridad
+	  Bajo      Solo caracteres | Maximo 8 caracteres
+	  Meido     Caracters y Numeros | Minimo 9 Caracteres
+	  Alto      Caracteres, Numeros y Simbolos | Minimo 12 Caracteres
+	  Muy Alto  Caracteres, Numeros y Simbolos | Minimo 16 Caracteres
+	  */
+	  
+
 	  if (estaId === "password") {
 		if (numCaracteres > 0 && numCaracteres < nivelBajo) {
 		  nivel = "bajo";
 				  espanNivelesColores.removeClass().addClass("spanNivelesColores bajo");        
 		} 
-		else if (numCaracteres >= nivelBajo && numCaracteres < nivelMedio) {
+		else if (numCaracteres >= nivelBajo && numCaracteres < nivelMedio && password.match(/[A-Z,a-z,0-9]/g) ) {
 		  nivel = "medio";
 				  espanNivelesColores.removeClass().addClass("spanNivelesColores medio"); 
 		} 
-		else if (numCaracteres >= nivelMedio && numCaracteres < nivelAlto) {
+		else if (numCaracteres >= nivelMedio && numCaracteres < nivelAlto && password.match(/[A-Z,a-z,0-9]/g) && password.match(/(.*[!,@,#,$,%,^,&,*,?,_,~])/) ) {
 		  nivel = "alto";
 				  espanNivelesColores.removeClass().addClass("spanNivelesColores alto"); 
 		} 
-		else if (numCaracteres >= nivelAlto) {
+		else if (numCaracteres >= nivelAlto && password.match(/[A-Z,a-z,0-9]/g) && password.match(/(.*[!,@,#,$,%,^,&,*,?,_,~])/) ){
 		  nivel = "muy alto";
 				  espanNivelesColores.removeClass().addClass("spanNivelesColores muyAlto");
 		}
