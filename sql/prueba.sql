5b48cec6a68eaa2fa04540feef2d1351792564c3
ydpard43 <61339534+ydpard43@users.noreply.github.com>
2020-11-12 10:13:01 -0500

    Update prueba.sql
    Add table forum

--- sql/prueba.sql
+++ sql/prueba.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-11-2020 a las 23:17:03
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.2
+-- Tiempo de generación: 12-11-2020 a las 16:11:05
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,21 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `prueba`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `forum`
+--
+
+CREATE TABLE `forum` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  `entry` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  `user` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -38,20 +52,28 @@ CREATE TABLE `users` (
   `forgot_pass_identity` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
-  `status` enum('1','0') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1'
+  `status` enum('1','0') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1',
+  `permits` enum('1','11','111','1111') COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `phone`, `forgot_pass_identity`, `created`, `modified`, `status`) VALUES
-(3, 'Osman Armando', 'Cortes', 'jocker2198@gmail.com', '3ec57495a9fd90fe637c3661d5dee74e', '3222050561', '9a3b97954c27709cc65dd6ff0413a1e3', '2020-11-04 19:59:26', '2020-11-04 23:07:24', '1'),
-(7, 'Osman Armando', 'Jimenez Cortes', 'osman98125@gmail.com', '26eab58bf9538c754f185feb0112a9f0', '3222050561', '', '2020-11-04 22:02:21', '2020-11-04 22:02:21', '1');
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `phone`, `forgot_pass_identity`, `created`, `modified`, `status`, `permits`) VALUES
+(7, 'Osman Armando', 'Jimenez Cortes', 'osman98125@gmail.com', '26eab58bf9538c754f185feb0112a9f0', '3222050561', '', '2020-11-04 22:02:21', '2020-11-08 17:17:23', '1', '1'),
+(8, 'Yohan D', 'Pardo Pinzon', 'pardoyohan@gmail.com', '926beffbf047b7abe0912826427f0dbc', '3123456', '551a92ce8614fc0b9a0b4f07d2037bef', '2020-11-06 16:32:26', '2020-11-07 16:24:59', '1', '1111'),
+(10, 'saopiaoisa', 'saklasjkas', 'pinzonyohan@gmail.com', '926beffbf047b7abe0912826427f0dbc', '12345678', 'bd2ec8843cb1db1e6abebc8782c8b1f2', '2020-11-10 22:01:30', '2020-11-10 22:24:57', '1', '1111');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `forum`
+--
+ALTER TABLE `forum`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `users`
@@ -64,10 +86,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `forum`
+--
+ALTER TABLE `forum`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
