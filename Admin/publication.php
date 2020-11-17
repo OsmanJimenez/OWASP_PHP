065ab4ecf9da303693406db3bf191c93c97511f4
Osman Armando Jimenez Cortes <33326926+OsmanJimenez@users.noreply.github.com>
2020-11-17 14:22:02 -0500

    Fix_Design

--- Admin/publication.php
+++ Admin/publication.php
@@ -76,12 +76,11 @@ if(!empty($sessData['status']['msg'])){
         <div class="header-body">
           <div class="row align-items-center py-4">
             <div class="col-lg-6 col-7">
-              <h6 class="h2 text-white d-inline-block mb-0">Tables</h6>
+              <h6 class="h2 text-white d-inline-block mb-0">Publicaciones</h6>
               <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                 <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                   <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
-                  <li class="breadcrumb-item"><a href="#">Tables</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Tables</li>
+                  <li class="breadcrumb-item"><a href="#">Listar</a></li>
                 </ol>
               </nav>
             </div>
